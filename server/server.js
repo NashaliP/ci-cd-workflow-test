5be4e07e6a076dbf8a1fd5ea46d969fad944ea34
NashaliP <nashaliperera0@gmail.com>
2024-04-20 00:27:21 +0530

    refactor : dummy 2

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 80;
 const server = http.createServer(app);
 
 app.get("/",(req,res)=>{
-    res.send("Hello World!Welcome hello ayubowan you guysss whats good homiesssss!");
+    res.send("Hello World!Welcome hello  you guysss whats good homiesssss!");
 })
 
 server.listen(PORT,()=>{
