a25698a175312e108d67ba56044cf249bd2e4d71
NashaliP <nashaliperera0@gmail.com>
2024-04-20 00:22:46 +0530

    refactor:api endpoint

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 80;
 const server = http.createServer(app);
 
 app.get("/",(req,res)=>{
-    res.send("Hello World!Welcome you guysss!");
+    res.send("Hello World!Welcome hello ayubowan you guysss whats good homiesssss!");
 })
 
 server.listen(PORT,()=>{
-    console.log(`Server is now running on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 })
 
